e01e9a20d4baa276b895f942b1833d206f9e2d35
Berke Öztürk <59619850+Weapfrog@users.noreply.github.com>
2023-01-31 16:53:53 +0300

    .Core layer update

---
+++ RandomWordGeneratorGET.Core/Common/ConnectionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWordGeneratorGET.Core
+{
+    class ConnectionHelper
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return "Data Source=ANOOBREKHAN;Initial Catalog=RandomWordGeneratorDb;Integrated Security=True";
+            }
+        }
+    }
+}
---
+++ RandomWordGeneratorGET.Core/DapperHelper.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using RandomWordGeneratorGET.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWordGeneratorGET.Core
+{
+    public class DapperHelper
+    {
+        private readonly string ConnectionString;
+        SqlConnection conn;
+        public DapperHelper()
+        {
+            ConnectionString = ConnectionHelper.ConnectionString;
+            this.conn = new SqlConnection(ConnectionString);
+        }
+        public List<Words> GetWord()
+        {
+            try
+            {
+                if (this.conn.State != System.Data.ConnectionState.Open)
+                {
+                    this.conn.Close();
+                    this.conn.Open();
+                }
+
+
+                return conn.Query<Words>("SELECT TOP 1 * FROM Words ORDER BY WordID DESC", null, null, true, null, System.Data.CommandType.Text).ToList();
+
+            }
+            catch (Exception ex)
+            {
+                this.conn.Close();
+                return (new List<Words>());
+            }
+        }
+        public List<Words> GetAllWords()
+        {
+            try
+            {
+                if (this.conn.State == System.Data.ConnectionState.Closed)
+                    this.conn.Open();
+
+                return conn.Query<Words>("SELECT * FROM Words", null, null, true, null, System.Data.CommandType.Text).ToList();
+
+            }
+            catch (Exception ex)
+            {
+                this.conn.Close();
+                return (new List<Words>());
+            }
+
+        }
+    }
+}
