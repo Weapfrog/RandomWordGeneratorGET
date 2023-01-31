837e59d577c8a5872d94d1d1e5a1455abe7b2007
Berke Öztürk <59619850+Weapfrog@users.noreply.github.com>
2023-01-31 16:53:06 +0300

    .Business layer update

---
+++ RandomWordGeneratorGET.Business/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using RandomWordGeneratorGET.Core;
+using RandomWordGeneratorGET.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWordGeneratorGET.Business
+{
+    public class UnitOfWork
+    {
+        DapperHelper helper = new DapperHelper();
+        public UnitOfWork()
+        {
+
+        }
+        public List<Words> GetWords()
+        {
+            try
+            {
+                return helper.GetWord();
+            }
+            catch (Exception ex)
+            {
+                return new List<Words>();
+            }
+        }
+        public List<Words> GetAllWords()
+        {
+            try
+            {
+                return helper.GetAllWords();
+            }
+            catch (Exception ex)
+            {
+                return new List<Words>();
+            }
+        }
+    }
+}
