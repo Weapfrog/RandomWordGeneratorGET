15fcf21c2a02a917eea9f059554466f9d8409b84
Berke Öztürk <muzgash_berke@hotmail.com>
2023-01-31 16:52:21 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace RandomWordGeneratorGET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.backgroundWorkerWord = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(130, 316);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // backgroundWorkerWord
+            // 
+            this.backgroundWorkerWord.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerWord_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(157, 341);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "RANDOM WORD GENERATOR GET";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerWord;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using RandomWordGeneratorGET.Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomWordGeneratorGET
+{
+    public partial class Form1 : Form
+    {
+        UnitOfWork _uow = new UnitOfWork();
+        public Form1()
+        {
+            InitializeComponent();
+            Setup();
+        }
+        public void Setup()
+        {
+            listView1.Columns.Add("Code");
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            Control.CheckForIllegalCrossThreadCalls = false;
+            backgroundWorkerWord.WorkerReportsProgress = true;
+            backgroundWorkerWord.WorkerSupportsCancellation = true;
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        public int GetAllWordsCount()
+        {
+            var response = _uow.GetAllWords();
+            var count = response.Count;
+            return count;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var response = _uow.GetWords();
+            listView1.Items.Add(response[0].WordName);
+            if (backgroundWorkerWord.IsBusy != true)
+            {
+                backgroundWorkerWord.RunWorkerAsync();
+            }
+        }
+        public void AddListViewCode()
+        {
+            int index = listView1.Items.Count - 1;
+            var response = _uow.GetWords();
+            string code = response[0].WordName;
+            if (listView1.Items[index].Text != code)
+            {
+                listView1.Items.Add(code);
+                index++;
+            }
+        }
+
+        private void backgroundWorkerWord_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            while (true)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    AddListViewCode();
+                    Thread.Sleep(3000);
+                }
+            }
+        }
+    }
+}
